a40b967c22b75d9c43e657869fc537c62ebf2939
monierjimenez <monierjimenez@gmail.com>
2021-07-18 20:19:59 -0600

    Ruta modificada

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
     Route::get('/', function () {
         return view('welcome');
     });
-
+hola
     Auth::routes();
     Route::get('/home', 'HomeController@index')->name('home');
 
